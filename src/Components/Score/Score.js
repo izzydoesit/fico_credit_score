a8e22877c27dacd9ba43cf09a17111b0bb40dbb9
izzydoesit <israeldmatos@gmail.com>
2018-09-03 14:47:49 -0700

    Add classes + styling to Score component

--- src/Components/Score/Score.js
+++ src/Components/Score/Score.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
+import Chart from '../Chart/Chart';
 import './Score.css';
 
 export default class Score extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      score: '743',
-      rating: 'excellent'
+      score: '743', // stubbed, intent to set from incoming props
+      rating: 'excellent' // calculation based on what range score falls into
     }
   }
 
@@ -14,8 +15,9 @@ export default class Score extends Component {
     return(
       <div className="score-wrapper">
 
-        <h1>{this.state.score}</h1>
-        <p>{this.state.rating.toUpperCase()}</p>
+        <h1 className="score">{this.state.score}</h1>
+        <p className="rating">{this.state.rating.toUpperCase()}</p>
+        <p className="score-exp">You have excellent credit and your credit ranks in the top 25% percentile</p>
       </div>
     )
   }
