855d477b1941b7ae08d9a1200b2435194ea1d0b3
izzydoesit <israeldmatos@gmail.com>
2018-09-03 14:47:10 -0700

    Add factorlist + tiplist components w/ data props to App

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -3,16 +3,18 @@ import NavBar from '../NavBar/NavBar'
 import Score from '../Score/Score'
 import FactorList from '../FactorList/FactorList'
 import TipList from '../TipList/TipList'
+import { FACTORS, TIPS } from '../../data';
 import './App.css';
 
 class App extends Component {
+
   render() {
     return (
       <div className="App">
         <NavBar />
         <Score />
-        <FactorList />
-        <TipList />
+        <FactorList factors={FACTORS}/>
+        <TipList tips={TIPS} />
       </div>
     );
   }
